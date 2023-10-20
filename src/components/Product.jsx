e227a313657c0cdef0060bf4cecaaff17e09c94e
Dhruv Gupta <dhruv035@gmail.com>
2023-10-21 01:24:10 +0530

    Another Attempt

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -44,7 +44,7 @@ const Product = () => {
     };
     try {
       const response = await fetch(
-        `/api/transaction`,
+        `https://front-git-test-way2pay.vercel.app/api/transaction`,
         {
           method: "POST",
           headers: {
